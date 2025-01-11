cf72a5c8185050d4c860afa0be611c256c2446ed
Ray <raysan5@gmail.com>
2025-01-11 19:37:08 +0100

    Update gui_file_dialogs.h

--- src/gui_file_dialogs.h
+++ src/gui_file_dialogs.h
@@ -123,16 +123,16 @@ int GuiFileDialog(int dialogType, const char *title, char *fileName, const char 
         case DIALOG_OPEN_FILE: /* TODO: Load file modal dialog */ break;
         case DIALOG_OPEN_FILE_MULTI: /* TODO: Load multiple files modal dialog */ break;
         case DIALOG_OPEN_DIRECTORY: /* TODO: Load directory modal dialog */ break;
-        case DIALOG_SAVE_FILE: /* TODO: Load file modal dialog */ break;
+        case DIALOG_SAVE_FILE: /* TODO: Save file modal dialog */ break;
         case DIALOG_MESSAGE: result = GuiMessageBox((Rectangle){ GetScreenWidth()/2 - 160, GetScreenHeight()/2 - 120, 320, 120 }, title, message, filters); break;
         case DIALOG_TEXTINPUT: result = GuiTextInputBox((Rectangle){ GetScreenWidth()/2 - 160, GetScreenHeight()/2 - 120, 320, 120 }, title, message, filters, fileName, 512, NULL); break;
         default: break;
     }
 #else   // Use native OS dialogs (tinyfiledialogs)
 
-    const char *tempFileName = NULL;
+    char *tempFileName = NULL;
     int filterCount = 0;
-    const char **filterSplit = TextSplit(filters, ';', &filterCount);
+    char **filterSplit = TextSplit(filters, ';', &filterCount);
     
     switch (dialogType)
     {
