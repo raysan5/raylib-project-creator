09886685b691da120d121971e1af310ed9474060
Ray <raysan5@gmail.com>
2025-11-26 11:36:08 +0100

    Fix #13

--- src/template/projects/scripts/build.bat
+++ src/template/projects/scripts/build.bat
@@ -7,20 +7,19 @@ cd %~dp0
 :: .
 :: > Compile simple .rc file
 :: ----------------------------
-cmd /c windres ..\..\src\project_name.rc -o ..\..\src\project_name.rc.data
+cd ..\..\src
+cmd /c windres project_name.rc -o project_name.rc.data
 :: .
 :: > Generating project
 :: --------------------------
-cmd /c mingw32-make -f ..\..\src\Makefile ^
+cmd /c mingw32-make -f Makefile ^
 PROJECT_NAME=project_name ^
 PROJECT_VERSION=1.0 ^
 PROJECT_DESCRIPTION="ProjectDescription" ^
 PROJECT_INTERNAL_NAME=project_name ^
 PROJECT_PLATFORM=PLATFORM_DESKTOP ^
 PROJECT_SOURCE_FILES="project_name.c" ^
-BUILD_MODE="RELEASE" ^
-BUILD_WEB_ASYNCIFY=FALSE ^
-BUILD_WEB_MIN_SHELL=TRUE ^
-BUILD_WEB_HEAP_SIZE=268435456 ^
-RAYLIB_MODULE_AUDIO=TRUE ^
-RAYLIB_MODULE_MODELS=TRUE
+BUILD_MODE="RELEASE"
+:: > Return to scripts directory
+:: -----------------------------
+cd ..\projects\scripts
